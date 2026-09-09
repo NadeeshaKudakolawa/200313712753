07e7461349bfc61d36e1886f2c107db1676b6ce5
savidyakolonne <savidya.kolonne@gmail.com>
2026-09-09 12:15:50 +0530

    delte folder task2

--- task2/training-capacity/src/main/java/com/example/trainingcapacity/controller/TrainingProgramController.java
+++
@@ -1,4 +0,0 @@
-package com.example.trainingcapacity.controller;
-
-public class TrainingProgramController {
-}
--- task2/training-capacity/src/main/java/com/example/trainingcapacity/dto/TrainingProgramRequest.java
+++
@@ -1,4 +0,0 @@
-package com.example.trainingcapacity.dto;
-
-public class TrainingProgramRequest {
-}
