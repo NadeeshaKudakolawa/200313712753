7c1b816175ba9dad98927a9ec475c36bd96fee0e
savidyakolonne <savidya.kolonne@gmail.com>
2026-09-09 13:14:24 +0530

    feat: name chnged

--- task1/frontend/src/App.tsx
+++ task1/frontend/src/App.tsx
@@ -130,7 +130,7 @@ function App() {
         <div className="max-w-7xl mx-auto px-6 py-6">
 
           <h1 className="text-2xl font-bold">
-            Government Training Management System
+            Training Management System
           </h1>
 
           <p className="text-blue-100 mt-1">
